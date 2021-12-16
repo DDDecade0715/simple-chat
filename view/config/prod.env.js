9d4eda5ea2333fbbe3afb20ce180792bd4abe0f8
yingzhan <519203699@qq.com>
2021-12-16 18:31:40 +0800

    add:聊天原型

--- view/config/prod.env.js
+++ view/config/prod.env.js
@@ -1,7 +1,7 @@
 'use strict'
 module.exports = {
   NODE_ENV: "'production'",
-  BASE_API: "'http://182.254.245.219:8502/'",
-  WS_URL: "'ws://182.254.245.219:8502/ws'",
+  BASE_API: "'http://127.0.0.1:8502/'",
+  WS_URL: "'ws://127.0.0.1:8502/ws'",
   isDev: false
 }
